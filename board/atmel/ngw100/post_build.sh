b5a4431f4a5b57f6bd46b751b59e366b47a992d4
Tobias Schramm <tobias@t-sys.eu>
2021-08-24 22:27:52 +0200

    board: atmel: generate sdcard images for NGW100

---
+++ board/atmel/ngw100/post_build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+BOARD_DIR="$(dirname $0)"
+
+trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
+ROOTPATH_TMP="$(mktemp -d)"
+
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+rm -rf "${GENIMAGE_TMP}"
+
+genimage \
+	--rootpath "${ROOTPATH_TMP}"   \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${BOARD_DIR}/genimage.cfg"
